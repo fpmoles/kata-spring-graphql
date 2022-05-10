499f0bc2ff49effbbeda7214c96ce9d628182d03
Frank P Moley III <frankmoley@gmail.com>
2022-05-09 20:14:33 -0500

    initial project structure

---
+++ src/main/java/com/frankmoley/kata/firearmsgraphhql/FirearmsGraphhqlApplication.java
@@ -0,0 +1,13 @@
+package com.frankmoley.kata.firearmsgraphhql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirearmsGraphhqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirearmsGraphhqlApplication.class, args);
+	}
+
+}
